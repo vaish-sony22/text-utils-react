4d2470001b38d454fc5d96c89d66ac04a99a40ff
vaish-sony22 <vaishnavigandyadapu@gmail.com>
2024-11-12 17:52:20 +0530

    happy coding

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -59,6 +59,7 @@ export default function TextForm(props) {
             <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
+            <p>Happy coding...</p>
         </div>
         </>
     )
