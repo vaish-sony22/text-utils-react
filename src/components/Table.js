8ba295d2d659175ed21007ac87cb498dcbe9c750
vaish-sony22 <vaishnavigandyadapu@gmail.com>
2024-11-19 15:13:44 +0530

    table change

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,115 +1,74 @@
 import React from "react";
 
-export default function Table() {
-  const tableStyle = {
-    border: "1px solid black",
-    borderCollapse: "collapse",
-    width: "100%",
-    marginTop: "20px",
-    backgroundColor: "#f9f9f9",
-  };
-
-  const thStyle = {
-    backgroundColor: "rgb(0 156 255)",
-    color: "white",
-    padding: "8px",
-    textAlign: "center",
-  };
-
-  const tdStyle = {
-    padding: "8px",
-    textAlign: "center",
-  };
-
-  const tdStyleBlue ={
-    backgroundColor: "rgb(0 156 255)",
-    color: "white",
-    padding: "8px",
-    textAlign: "center",
-  }
-
-  const tdStyleRed ={
-    backgroundColor: "red",
-    color: "white",
-    padding: "8px",
-    textAlign: "center",
-  }
-
-  const tdStyleGreen ={
-    backgroundColor: "green",
-    color: "white",
-    padding: "8px",
-    textAlign: "center",
-  }
-
+export default function Table(props) {
   return (
     <div style={{ margin: "20px auto", maxWidth: "90%" }}>
-      <table style={tableStyle}>
+      <table style={props.tableStyle}>
         <thead>
           <tr>
-            <th style={thStyle}>Items</th>
-            <th style={thStyle}>Purchase Date</th>
-            <th style={thStyle}>Units Sold</th>
-            <th style={thStyle}>Unit Price</th>
-            <th style={thStyle}>Revenue</th>
-            <th style={thStyle}>Cost</th>
-            <th style={thStyle}>Profit</th>
+            <th style={props.thStyle}>Items</th>
+            <th style={props.thStyle}>Purchase Date</th>
+            <th style={props.thStyle}>Units Sold</th>
+            <th style={props.thStyle}>Unit Price</th>
+            <th style={props.thStyle}>Revenue</th>
+            <th style={props.thStyle}>Cost</th>
+            <th style={props.thStyle}>Profit</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td style={tdStyle}>Drinks</td>
-            <td style={tdStyle}>10/06/2014</td>
-            <td style={tdStyle}>4,382.00</td>
-            <td style={tdStyle}>47.45</td>
-            <td style={tdStyleBlue}>$207,925.90</td>
-            <td style={tdStyleRed}>$139,303.78</td>
-            <td style={tdStyleGreen}>$68,622.12</td>
+            <td style={props.tdStyle}>Drinks</td>
+            <td style={props.tdStyle}>10/06/2014</td>
+            <td style={props.tdStyle}>4,382.00</td>
+            <td style={props.tdStyle}>47.45</td>
+            <td style={props.tdStyleBlue}>$207,925.90</td>
+            <td style={props.tdStyleRed}>$139,303.78</td>
+            <td style={props.tdStyleGreen}>$68,622.12</td>
           </tr>
           <tr>
-            <td style={tdStyle}>Drinks</td>
-            <td style={tdStyle}>10/06/2014</td>
-            <td style={tdStyle}>4,382.00</td>
-            <td style={tdStyle}>47.45</td>
-            <td style={tdStyleBlue}>$207,925.90</td>
-            <td style={tdStyleRed}>$139,303.78</td>
-            <td style={tdStyleGreen}>$68,622.12</td>
+            <td style={props.tdStyle}>Drinks</td>
+            <td style={props.tdStyle}>10/06/2014</td>
+            <td style={props.tdStyle}>4,382.00</td>
+            <td style={props.tdStyle}>47.45</td>
+            <td style={props.tdStyleBlue}>$207,925.90</td>
+            <td style={props.tdStyleRed}>$139,303.78</td>
+            <td style={props.tdStyleGreen}>$68,622.12</td>
           </tr>
           <tr>
-            <td style={tdStyle}>Drinks</td>
-            <td style={tdStyle}>10/06/2014</td>
-            <td style={tdStyle}>4,382.00</td>
-            <td style={tdStyle}>47.45</td>
-            <td style={tdStyleBlue}>$207,925.90</td>
-            <td style={tdStyleRed}>$139,303.78</td>
-            <td style={tdStyleGreen}>$68,622.12</td>
+            <td style={props.tdStyle}>Drinks</td>
+            <td style={props.tdStyle}>10/06/2014</td>
+            <td style={props.tdStyle}>4,382.00</td>
+            <td style={props.tdStyle}>47.45</td>
+            <td style={props.tdStyleBlue}>$207,925.90</td>
+            <td style={props.tdStyleRed}>$139,303.78</td>
+            <td style={props.tdStyleGreen}>$68,622.12</td>
           </tr>
           <tr>
-            <td style={tdStyle}>Drinks</td>
-            <td style={tdStyle}>10/06/2014</td>
-            <td style={tdStyle}>4,382.00</td>
-            <td style={tdStyle}>47.45</td>
-            <td style={tdStyleBlue}>$207,925.90</td>
-            <td style={tdStyleRed}>$139,303.78</td>
-            <td style={tdStyleGreen}>$68,622.12</td>
+            <td style={props.tdStyle}>Drinks</td>
+            <td style={props.tdStyle}>10/06/2014</td>
+            <td style={props.tdStyle}>4,382.00</td>
+            <td style={props.tdStyle}>47.45</td>
+            <td style={props.tdStyleBlue}>$207,925.90</td>
+            <td style={props.tdStyleRed}>$139,303.78</td>
+            <td style={props.tdStyleGreen}>$68,622.12</td>
           </tr>
           <tr>
-            <td style={tdStyle}>Drinks</td>
-            <td style={tdStyle}>10/06/2014</td>
-            <td style={tdStyle}>4,382.00</td>
-            <td style={tdStyle}>47.45</td>
-            <td style={tdStyleBlue}>$207,925.90</td>
-            <td style={tdStyleRed}>$139,303.78</td>
-            <td style={tdStyleGreen}>$68,622.12</td>
+            <td style={props.tdStyle}>Drinks</td>
+            <td style={props.tdStyle}>10/06/2014</td>
+            <td style={props.tdStyle}>4,382.00</td>
+            <td style={props.tdStyle}>47.45</td>
+            <td style={props.tdStyleBlue}>$207,925.90</td>
+            <td style={props.tdStyleRed}>$139,303.78</td>
+            <td style={props.tdStyleGreen}>$68,622.12</td>
           </tr>
           <tr>
-            <td style={tdStyle}>Drinks</td>
-            <td style={tdStyle}>10/06/2014</td>
-            <td style={tdStyle}>4,382.00</td>
-            <td style={tdStyle}>47.45</td>
-            <td style={tdStyleBlue}>$207,925.90</td>
-            <td style={tdStyleRed}>$139,303.78</td>
-            <td style={tdStyleGreen}>$68,622.12</td>
+            <td style={props.tdStyle}>Drinks</td>
+            <td style={props.tdStyle}>10/06/2014</td>
+            <td style={props.tdStyle}>4,382.00</td>
+            <td style={props.tdStyle}>47.45</td>
+            <td style={props.tdStyleBlue}>$207,925.90</td>
+            <td style={props.tdStyleRed}>$139,303.78</td>
+            <td style={props.tdStyleGreen}>$68,622.12</td>
           </tr>
           
         </tbody>
